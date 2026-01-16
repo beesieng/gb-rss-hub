01684a949d40f1da0aa5715491f2ddfb74f3a222
shaojiaxing <77831808@qq.com>
2026-01-16 16:16:10 +0800

    feat(archdaily): add ArchDaily RSS feed route and handler
    - Implemented route for ArchDaily
    - Added RSS feed parsing functionality
    - Included metadata and item mapping

---
+++ lib/routes-beeshub/archdaily/namespace.ts
@@ -0,0 +1,6 @@
+export default {
+    name: 'ArchDaily',
+    url: 'https://www.archdaily.com/',
+    categories: ['design'],
+    description: 'ArchDaily | Broadcasting Architecture Worldwide',
+};
